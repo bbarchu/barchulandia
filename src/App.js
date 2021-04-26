0bf997cc4b21c1d67875f25f0b82b8dcda8db6b8
Bárbara <bmesones@fi.uba.ar>
2021-04-26 19:45:25 -0300

    ejercicios primera entrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>Barchulandia</p>
+        <a className="App-link" href="/sistemaSolar">
+          Sistema solar
+        </a>
+        <a className="App-link" href="/letras1">
+          Demo letras - ej1
+        </a>
+        <a className="App-link" href="/letras2">
+          Demo letras - ej2
         </a>
       </header>
+      <Router>
+        <Switch>
+          <Route
+            path="/sistemaSolar"
+            render={() => {
+              window.location.href = "ejercicioSistemaSolar/sistemaSolar.html";
+            }}
+          />
+          <Route
+            path="/letras1"
+            render={() => {
+              window.location.href = "demoLetras3D/ejercicio1.html";
+            }}
+          />
+          <Route
+            path="/letras2"
+            render={() => {
+              window.location.href = "demoLetras3D/ejercicio2.html";
+            }}
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
