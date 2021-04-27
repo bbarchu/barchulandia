559aebace39dd8318e23b3cff1fb599d21655e3a
Bárbara <bmesones@fi.uba.ar>
2021-04-27 19:18:38 -0300

    cambios ej2letras y otros

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
@@ -8,17 +8,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Barchulandia</p>
-        <a className="App-link" href="/sistemaSolar">
-          Sistema solar
-        </a>
-        <a className="App-link" href="/letras1">
-          Demo letras - ej1
-        </a>
-        <a className="App-link" href="/letras2">
-          Demo letras - ej2
-        </a>
       </header>
       <Router>
+        <ul>
+          <li>
+            <Link to="/sistemaSolar">Sistema solar</Link>
+          </li>
+          <li>
+            <Link to="/letras1">Demo letras - ej1</Link>
+          </li>
+          <li>
+            <Link to="/letras2">Demo letras - ej2</Link>
+          </li>
+        </ul>
+
         <Switch>
           <Route
             path="/sistemaSolar"
